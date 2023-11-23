2eee9ae955ec834f43961d1eaff47f457eae0599
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2023-11-23 09:54:07 +0100

    Added the delete in dogcontroller, a command and a commandhandler

---
+++ Application/Commands/Dogs/DeleteDog/DeleteDogByIdCommand.cs
@@ -0,0 +1,22 @@
+﻿using Application.Dtos;
+using Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Dogs.DeleteDog
+{
+    public class DeleteDogByIdCommand : IRequest<bool>
+    {
+        public DeleteDogByIdCommand(DogDto deletedDog, Guid deletedDogId)
+        {
+            DeletedDog = deletedDog;
+            DeletedDogId = deletedDogId;
+        }
+        public DogDto DeletedDog { get; }
+        public Guid DeletedDogId { get; }
+    }
+}
